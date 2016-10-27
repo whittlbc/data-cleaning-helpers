f67e427a01212445d567e62359f9bdcb977bba3f
Ben Whittle <benWhittle@DN51sder.SUNet>
2016-10-26 18:30:16 -0700

    modifying combine_nld_json file to take a directory argument

--- combine_nld_json.py
+++ combine_nld_json.py
@@ -1,21 +1,26 @@
 from os import listdir
 from os.path import isfile, join
 import json
+import sys
 
-data_path = 'data'
+if len(sys.argv) < 2:
+	raise "No data directory specified\nUsage: $ python combine_nld_json my_data_dir"
+
+data_path = sys.argv[1]
 files = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 
 entries_map = {}
 
 for file in files:
-	
+
 	with open('{}/{}'.format(data_path, file), 'r') as f:
-		
+
 		for str_entry in f.read().split("\n"):
-			
+
 			if str_entry.strip():
 				json_entry = json.loads(str_entry)
 				entries_map[json_entry['id']] = json_entry
-			
+
+
 with open('data.json', 'w+') as f:
 	json.dump(entries_map, f)
