5f99b6cec47b53fa93e86a6d1958e7f9378afb26
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-10-27 15:53:24 -0700

    adding urllib2 to allow for file fetching at remote urls

--- nld_json_to_db.py
+++ nld_json_to_db.py
@@ -2,12 +2,14 @@
 # Insert new-line-delimited, stringified JSON entries from file into mongodb collection
 
 import json
+import urllib2
 from pymongo import MongoClient
 
 client = MongoClient()
 collection = client['data-helpers']['entries']
 
-with open('hn_compose') as f:
-	entries = [entry for entry in f.read().split("\n") if entry.strip() != '']
-	collection.insert(json.loads('[' + ','.join(entries) + ']'))
-	f.close()
+composite_file_url = 'https://storage.googleapis.com/jarvis123192/hn_compose'
+
+entries = [entry for entry in urllib2.urlopen(composite_file_url).read().split("\n") if entry.strip() != '']
+
+collection.insert(json.loads('[' + ','.join(entries) + ']'))
