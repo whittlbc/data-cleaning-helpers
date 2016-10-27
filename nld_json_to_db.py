35a625072feb1f5c4b96762b62c4207b62608d62
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-10-27 15:18:42 -0700

    modified nld_json_to_db file to assume a composite file is already in existence locally

--- nld_json_to_db.py
+++ nld_json_to_db.py
@@ -1,19 +1,13 @@
-from os import listdir
-from os.path import isfile, join
+
+# Insert new-line-delimited, stringified JSON entries from file into mongodb collection
+
 import json
-import sys
 from pymongo import MongoClient
 
 client = MongoClient()
 collection = client['data-helpers']['entries']
 
-if len(sys.argv) < 2:
-	raise "No data directory specified\nUsage: $ python combine_nld_json my_data_dir"
-
-data_path = sys.argv[1]
-files = [f for f in listdir(data_path) if isfile(join(data_path, f))]
-
-for file in files:
-	with open('{}/{}'.format(data_path, file), 'r') as f:
-		collection.insert(json.loads('[' + ','.join(filter(lambda x: x != '', f.read().split("\n"))) + ']'))
-		f.close()
+with open('hn_compose') as f:
+	entries = [entry for entry in f.read().split("\n") if entry.strip() != '']
+	collection.insert(json.loads('[' + ','.join(entries) + ']'))
+	f.close()
