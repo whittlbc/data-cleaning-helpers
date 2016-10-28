f55a161cd787afc71ffbfe093ec5dc3da12b9d29
Ben Whittle <benWhittle@rescomp-16-331629.stanford.edu>
2016-10-27 20:51:08 -0700

    updating file_url and db collection name

--- nld_json_to_db.py
+++ nld_json_to_db.py
@@ -6,10 +6,10 @@ import urllib2
 from pymongo import MongoClient
 
 client = MongoClient()
-collection = client['data-helpers']['entries']
+collection = client['dataset-db']['resources']
 
-composite_file_url = 'https://storage.googleapis.com/jarvis123192/hn_compose'
+file_url = 'https://storage.googleapis.com/jarvis123192/hn/hn000000000001'
 
-entries = [entry for entry in urllib2.urlopen(composite_file_url).read().split("\n") if entry.strip() != '']
+entries = [entry for entry in urllib2.urlopen(file_url).read().split("\n") if entry.strip() != '']
 
 collection.insert(json.loads('[' + ','.join(entries) + ']'))
