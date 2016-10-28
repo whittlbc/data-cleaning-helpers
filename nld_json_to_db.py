abd134a4b42ecca741c061f16f1d6e828644fe81
Ben Whittle <benWhittle@rescomp-16-331629.stanford.edu>
2016-10-27 22:09:57 -0700

    making script dynamically download all files from folder

--- nld_json_to_db.py
+++ nld_json_to_db.py
@@ -1,50 +1,23 @@
-
-# Insert new-line-delimited, stringified JSON entries from file into mongodb collection
-
+import os
 import json
 import urllib2
+import sys
 from pymongo import MongoClient
 
-client = MongoClient()
-collection = client['dataset-db']['resources']
 
-file_url = 'https://storage.googleapis.com/jarvis123192/hn/hn0000000000'
-
-suffix = [
-	'02',
-	'03',
-	'04',
-	'05',
-	'06',
-	'07',
-	'08',
-	'09',
-	'10',
-	'11',
-	'12',
-	'13',
-	'14',
-	'15',
-	'16',
-	'17',
-	'18',
-	'19',
-	'20',
-	'21',
-	'22',
-	'23',
-	'24',
-	'25',
-	'26',
-	'27',
-	'28',
-	'29',
-	'30',
-	'31'
-]
-
-for num in suffix:
-	url = file_url + num
-	entries = [entry for entry in urllib2.urlopen(url).read().split("\n") if entry.strip() != '']
-	collection.insert(json.loads('[' + ','.join(entries) + ']'))
-	entries = None
+if len(sys.argv) == 2:
+	client = MongoClient()
+	collection = client['dataset-db']['resources']
+	
+	tmp_file = 'files.txt'
+	
+	os.system('gsutil ls gs://jarvis123192/{} >> {}'.format(sys.argv[1], tmp_file))
+	
+	with open(tmp_file) as f:
+		urls = f.read().split("\n")[1:]
+		f.close()
+		
+	os.remove(tmp_file)
+		
+	for url in urls:
+		collection.insert(json.loads('[' + ','.join([e for e in urllib2.urlopen(url).read().split("\n") if e.strip() != '']) + ']'))
