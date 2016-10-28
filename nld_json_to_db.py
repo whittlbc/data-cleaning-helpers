bf0d05ecd760a767141ed9509bde102bf906d7fd
Ben Whittle <benWhittle@rescomp-16-331629.stanford.edu>
2016-10-27 21:10:36 -0700

    trying to use loops now to download all files from folder

--- nld_json_to_db.py
+++ nld_json_to_db.py
@@ -8,8 +8,43 @@ from pymongo import MongoClient
 client = MongoClient()
 collection = client['dataset-db']['resources']
 
-file_url = 'https://storage.googleapis.com/jarvis123192/hn/hn000000000001'
+file_url = 'https://storage.googleapis.com/jarvis123192/hn/hn0000000000'
 
-entries = [entry for entry in urllib2.urlopen(file_url).read().split("\n") if entry.strip() != '']
+suffix = [
+	'02',
+	'03',
+	'04',
+	'05',
+	'06',
+	'07',
+	'08',
+	'09',
+	'10',
+	'11',
+	'12',
+	'13',
+	'14',
+	'15',
+	'16',
+	'17',
+	'18',
+	'19',
+	'20',
+	'21',
+	'22',
+	'23',
+	'24',
+	'25',
+	'26',
+	'27',
+	'28',
+	'29',
+	'30',
+	'31'
+]
 
-collection.insert(json.loads('[' + ','.join(entries) + ']'))
+for num in suffix:
+	url = file_url + num
+	entries = [entry for entry in urllib2.urlopen(file_url).read().split("\n") if entry.strip() != '']
+	collection.insert(json.loads('[' + ','.join(entries) + ']'))
+	entries = None
