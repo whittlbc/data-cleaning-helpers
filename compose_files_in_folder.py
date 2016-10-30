1f6cc97e777a9fe7f43671d61f28f0f6b08f35b4
Ben Whittle <benwhittle31@gmail.com>
2016-10-29 22:07:32 -0700

    removing tmp file after done with it

--- compose_files_in_folder.py
+++ compose_files_in_folder.py
@@ -5,17 +5,21 @@
 import os
 import sys
 
-if len(sys.argv) == 3:
-	folder_path = sys.argv[1].strip()
-	compose_file_path = sys.argv[2].strip()
-	tmp_file_path = 'tmp/files.txt'
-	
-	if os.path.isfile(tmp_file_path):
-		os.remove(tmp_file_path)
-	
-	os.system('gsutil ls gs://{} >> {}'.format(folder_path, tmp_file_path))
-	
-	with open(tmp_file_path) as f:
-		files = ' '.join(f.read().split("\n")[1:])
-	
-	os.system('gsutil compose {} gs://{}'.format(files, compose_file_path))
+
+if len(sys.argv) != 3:
+	raise BaseException('Invalid args.\nUsage: python compose_files_in_folder <gs_folder_path> <composite_file_path>')
+
+folder_path = sys.argv[1].strip()
+compose_file_path = sys.argv[2].strip()
+tmp_file_path = 'tmp/files.txt'
+
+if os.path.isfile(tmp_file_path):
+	os.remove(tmp_file_path)
+
+os.system('gsutil ls gs://{} >> {}'.format(folder_path, tmp_file_path))
+
+with open(tmp_file_path) as f:
+	files = ' '.join(f.read().split("\n")[1:])
+
+os.system('gsutil compose {} gs://{}'.format(files, compose_file_path))
+os.remove(tmp_file_path)
