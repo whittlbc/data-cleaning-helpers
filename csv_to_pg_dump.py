385de22551c8909c15f2b889cd0c6d722834ed30
Ben Whittle <benwhittle31@gmail.com>
2016-10-29 21:22:00 -0700

    Fixed a bunch of type-related bugs and got script successfully working with a 3.5GB csv file --> created 1.5GB zipped up pg_dump.

--- csv_to_pg_dump.py
+++ csv_to_pg_dump.py
@@ -1,7 +1,7 @@
 from psycopg2 import connect
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import sys
-import re
+import pandas
 import os
 
 
@@ -34,6 +34,8 @@ def create_db(db):
 	if database_exists(cur, db):
 		error("Error: Database '{}' already exists.".format(db))
 	
+	print 'Creating database, {}...'.format(db)
+	
 	# Create the new DB by name
 	cur.execute('CREATE DATABASE {};'.format(db))
 
@@ -41,138 +43,124 @@ def create_db(db):
 	leave_db(cur, con)
 			
 
-def get_data_types(data, num_cols):
-	data_types = [None] * num_cols
+def get_data_types(df, headers):
+	data_types = []
 	
-	for row in data:
-		col_index = 0
+	for header in headers:
+		col_data = filter(lambda x: x != '', list(df.loc[:, header]))
 		
-		for value in row:
-			if not data_types[col_index] and value.strip():
-				data_types[col_index] = determine_data_type(value.strip())
+		if not col_data:
+			error('Column "{}" is completely empty.'.format(header))
+		
+		col_type = None
+		
+		for val in col_data:
+			if col_type != 'text':
+				try:
+					int(val)
+					col_type = 'int'
+				except ValueError:
+					if val.strip().lower() in ['true', 'false']:
+						col_type = 'boolean'
+					else:
+						col_type = 'text'
+	
+		data_types.append(col_type)
+	
+	return data_types
 
-			col_index += 1
-		
-		if data_types.count(None) == 0:
-			return data_types
-		
-	error('Error determining data types - One column never had any entries.')
-			
-		
+
 def determine_data_type(data):
-	# First check to see if data can be represented as an integer
 	try:
 		int(data)
-		return 'int'
+		dt = 'int'
 	except ValueError:
 		# Not an integer...so either text or boolean
-		if data in ['true', 'false']:
-			return 'boolean'
-		
-		return 'text'
+		if data.lower() in ['true', 'false']:
+			dt = 'boolean'
+		else:
+			dt = 'text'
 	
+	return dt
 
-def determine_primary_key(headers):
-	if 'id' in headers: return 'id'
-	
-	if 'row' in headers:
-		return 'row_num'
-	else:
-		return 'row'
+
+def read_csv(csv):
+	data = pandas.read_csv(csv, sep=',', header=0, encoding='utf-8', dtype=str)
+	data.fillna('', inplace=True)
+	return data
 
 
 def create_schema(csv):
-	# Download or open the csv
-	if re.search('^(http|https|ftp)', csv):
-		import urllib2
-		file = urllib2.urlopen(csv)
-	else:
-		file = open(csv)
-		
-	# Turn string csv data into chart-style data
-	data = map(lambda row: row.split(','), file.read().split("\n"))
+	print 'Reading csv...'
+	df = read_csv(csv)
+	data = df.values
+	headers = df.columns.values
 	
-	# Determine what your column names will be
-	headers = map(lambda h: h.lower().strip(), data[0])
-	data_rows = data[1:]
+	print 'Inferring datatypes...'
 	
-	# Determine data types for each column based on the data in the csv
-	data_types = get_data_types(data_rows, len(headers))
+	data_types = get_data_types(df, headers)
 	
 	headers_with_type = []
-	
-	j = 0
+	sql_schema = []
+
+	i = 0
 	for header in headers:
+		dt = data_types[i]
+		
 		headers_with_type.append({
 			'header': header,
-			'type': data_types[j]
+			'type': dt
 		})
-		j += 1
-	
-	# Determine the primary key (or if you'll need to add one)
-	primary_key = determine_primary_key(headers)
-	
-	sql_schema = []
-	
-	header_with_prim_key = headers
-	
-	# If primary column needs to be added, do that.
-	if primary_key in ['row', 'row_num']:
-		header_with_prim_key = [primary_key] + header_with_prim_key
-		data_types = ['int'] + data_types
 		
-	i = 0
-	for col_name in header_with_prim_key:
-		sql_col = '{} {}'.format(col_name, data_types[i])
-		
-		if col_name == primary_key:
-			sql_col += ' PRIMARY KEY NOT NULL'
-		
-		sql_schema.append(sql_col)
+		sql_schema.append('{} {}'.format(header, dt))
 		i += 1
 		
 	schema = ', '.join(sql_schema)
 	
-	return schema, headers_with_type, data_rows
+	return schema, headers_with_type, data
 	
 
-def create_table(db, csv):
+def create_and_populate_table(db, csv):
 	con = get_db_connection(db)
 	cur = con.cursor()
 	
 	if table_exists(cur, 'resources'):
 		error("Error: Table 'resources' already exists.")
-	
+				
 	# Create a schema based on our csv headers and data
 	# (Note: CSV must be comma-delimited for now)
-	schema, headers_with_type, data_rows = create_schema(csv)
+	schema, headers_with_type, data = create_schema(csv)
+	
+	print 'Creating resources table...'.format(db)
 	
 	# Create our 'resources' table
 	cur.execute('CREATE TABLE resources( {} );'.format(schema))
 	
-	batch_size = 1000
-	row_count = len(data_rows)
+	batch_size = 5000
+	row_count = len(data)
 	
+	print 'Populating resources table...'.format(db)
+		
 	if row_count < batch_size:
-		insert_table_data(cur, headers_with_type, data_rows)
+		insert_table_data(cur, headers_with_type, data)
 	else:
 		batches = row_count / batch_size
 		
 		if row_count % batch_size:
 			batches += 1
 		
-		for i in range(batches):
+		for i in xrange(batches):
 			range_start = batch_size * i
 			range_end = batch_size * (i + 1)
 			
 			if range_end > row_count:
-				batch = data_rows[range_start:]
+				batch = data[range_start:]
 			else:
-				batch = data_rows[range_start:range_end]
+				batch = data[range_start:range_end]
 		
 			insert_table_data(cur, headers_with_type, batch)
 			
-			print "Inserted: {}%".format(((i + 1) / batches) * 100)
+			print "Inserted batch {} of {}.".format((i + 1), batches)
 
 	leave_db(cur, con)
 
@@ -199,19 +187,20 @@ def insert_table_data(cur, headers, rows):
 
 
 def format_val(type, val):
-	# Handle what the 'null' value will be if value doesn't exist
+	# if no value, return the empty type you want
 	if not val:
-		if type =='text': return ''
-		return None
+		if type == 'text':
+			return ''
+		else:
+			return None
 	
-	# Value exists, so convert it to the correct type
 	if type == 'int':
 		val = int(val)
 	elif type == 'boolean':
 		val = bool(val)
 	else:
-		val = str(val)
-		
+		val = val.encode('utf-8')
+	
 	return val
 
 
@@ -220,12 +209,17 @@ def error(message):
 
 
 # Ensure required args passed in
-if len(sys.argv) not in [2, 3, 4]:
+if len(sys.argv) != 3:
 	error("Invalid args.\nUsage: $ python csv_to_pg_dump.py <dbname> <path_to_csv>")
 
 db_name = sys.argv[1].strip()
 csv_path = sys.argv[2].strip()
 
 create_db(db_name)
-create_table(db_name, csv_path)
+create_and_populate_table(db_name, csv_path)
+
+print 'Dumping database...'
+
 os.system('pg_dump {} | gzip > pgdump.gz'.format(db_name))
+
+print 'Created pgdump.gz from {}.'.format(csv_path)
